fa29514e8c54d3ba20b09cf3d910a444df63f7a3
rubens-lavor <haryson07@gmail.com>
2020-09-02 22:33:34 -0300

    backend concluído

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect('mongodb://localhost/todo',{
+module.exports = mongoose.connect('mongodb://localhost:27017/todo',{
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useFindAndModify: false,

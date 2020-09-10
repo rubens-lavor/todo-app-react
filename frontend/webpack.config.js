b638a5e25152b2544820dbc1b997410e34ba9fae
rubens-lavor <haryson07@gmail.com>
2020-09-09 22:47:12 -0300

    install react

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,27 +1,27 @@
-const webpack = require("webpack")
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './src/index.jsx',
     output: {
-        path: __dirname + "/public",
-        filename: './app'
+        path: __dirname + '/public',
+        filename: './app.js'
     },
     devServer: {
-        port:8080,
-        contentBase: './public'
+        port: 8080,
+        contentBase: './public',
     },
     resolve: {
-        extensions: ['','.js','.jsx'],
+        extensions: ['', '.js', '.jsx'],
         alias: {
             modules: __dirname + '/node_modules'
         }
     },
-    plugins: [
+    plugins: [ 
         new ExtractTextPlugin('app.css')
     ],
     module: {
-        loaders:[{
+        rules: [{
             test: /.js[x]?$/,
             loader: 'babel-loader',
             exclude: /node_modules/,
@@ -31,9 +31,9 @@ module.exports = {
             }
         }, {
             test: /\.css$/,
-            loader: ExtractTextPlugin.extract('style-loader','css-loader')
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
         }, {
-            test:/\.woff|.woff2|.ttf|.eot|.svg*.*$/,
+            test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
             loader: 'file'
         }]
     }

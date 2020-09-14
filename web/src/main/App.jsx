629e8497849879f533d75ad7a232b5ed75e6d997
rubens-lavor <haryson07@gmail.com>
2020-09-13 23:11:33 -0300

    componente menu

--- web/src/main/App.jsx
+++ web/src/main/App.jsx
@@ -3,9 +3,11 @@ import './App.css';
 
 import Todo from '../todo/todo'
 import About from '../about/about'
+import Menu from '../template/menu'
 
 export default props => (
   <div className="container">
+      <Menu />
       <Todo />
       <About />
   </div>

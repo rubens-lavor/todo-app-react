28a1e4a1e69e22df42022c7d699a0c58e0362f7f
rubens-lavor <haryson07@gmail.com>
2020-09-16 18:21:45 -0300

    init

--- web/src/template/menu.jsx
+++ web/src/template/menu.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { Navbar, Nav, Form } from 'reactstrap'
+import {NavDropdown, Button, FormControl} from 'bootstrap'
 
 export default props => (
+    /*
     <nav className='navbar navbar-inverse bg-inverse'>
         <div className="container">
             <div className="navbar-header">
@@ -16,4 +19,15 @@ export default props => (
             </div>
         </div>
     </nav>
+    */
+   <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
 )
